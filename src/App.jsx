5f7b67dd5a0b07d45e2545ab6a28bb736bff26e7
Matheus Telles <business.mwtelles@gmail.com>
2025-01-30 23:13:41 -0300

    feat: iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-[var(--color-blue)] flex-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
